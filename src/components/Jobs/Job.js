d158e2a7d7fa55a0100ec76ce5da3a348f26332a
sky cherfa <sherifcherfa@gmail.com>
2021-10-22 18:46:24 +0100

    fixed job section for mobile probably bugged the desktop version out tho

--- src/components/Jobs/Job.js
+++ src/components/Jobs/Job.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 import JobCard from './JobCard'
-
+import JobLogoCard from './JobLogoCard'
 import './Jobs.css'
 
 export default class Job extends Component {
@@ -19,9 +19,9 @@ export default class Job extends Component {
                 
 
                 <div className="scroller">
-                    <JobCard skill1={"https://i.imgur.com/vH6dBKz.png"} skillt1={"Media management"} skill2={"https://i.imgur.com/hn8WUCo.png"} skillt2={"Time Management"} skill4={"https://i.imgur.com/JJLma0c.png"} skill3={"https://i.imgur.com/PGjfwTi.png"} skillt3={"Sales"} skillt4={"Marketing"} title={"AMYSKY LTD"} para={"2 years ago my partner and I started an entertainment business online, it's been pretty successful and it continues to grow every day."}/>
-                    <JobCard skill1={"https://i.imgur.com/vH6dBKz.png"} skillt1={"Media management"} skill2={"https://i.imgur.com/hn8WUCo.png"} skillt2={"Time Management"} skill4={"https://i.imgur.com/oqK7CnH.png"} skill3={"https://i.imgur.com/BAc1iep.png"} skillt3={"Web Dev"} skillt4={"Responsive design"} title={"Cannanodes"} para={"A project intended to improve the life quality of medical Cannabis Patients in the UK, the project is still in progress"} />
-                    <JobCard skill1={"https://i.imgur.com/vH6dBKz.png"} skillt1={"Media management"} skill2={"https://i.imgur.com/hn8WUCo.png"} skillt2={"Time Management"} skill4={"https://i.imgur.com/oqK7CnH.png"} skill3={"https://i.imgur.com/BAc1iep.png"} skillt3={"Web Dev"} skillt4={"Responsive design"} title={"Portfolio"} para={"After self-studying web development for 2 years I decided it's time to try and break out into tech, which led me to creating my Portfolio"} />
+                    <JobLogoCard title={"Amysky LTD"} bc={" 2px solid pink"} btc={"pink"} img={"https://i.imgur.com/xIyggkg.png"} para={"2 years ago my partner and I started an entertainment business online, it's been pretty successful and it continues to grow every day."}/>
+                    <JobLogoCard title={"Cannanodes"} bc={" 2px solid #7c968d"} btc={"#7c968d"} img={"https://i.imgur.com/m186TRU.png"} para={"A project intended to improve the life quality of medical Cannabis Patients in the UK, the project is still in progress"}/>
+                    <JobLogoCard title={"Portfolio"} bc={" 2px solid #1efeff"} btc={"#1efeff"} img={"https://i.imgur.com/LyEk7u1.png"} para={"After self-studying web development for 2 years I decided it's time to try and break out into tech, which led me to creating my Portfolio"}/>
                 </div>
             </div>
         )
---
+++ src/components/Jobs/JobLogoCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './Jobs.css'
+
+export default function JobLogoCard(props) {
+    return (
+        <div className="job-card-wrapper" style={{border: props.bc}}>
+            <div className="top-card">
+                <h3 className="top-header-title">{props.title}</h3>
+
+                <img className="circular-img" src={props.img} alt="logo"></img>
+
+                <p className="top-c-para"> 
+                    {props.para}
+                </p>
+
+               
+            </div>
+
+
+            <div className="bottom-card">
+                <button className="btn-job" style={{border: props.bc, color: props.btc}}>Contact me</button>
+            </div>
+            
+        </div>
+    )
+}
